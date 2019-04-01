14a6d066321521146ffebf9102387b1fe9e95657
science162 <albertomouaffo@gmail.com>
2019-04-01 21:36:07 +0200

    first commit

--- src/main/java/gl51/projet/java/StudentController.java
+++ src/main/java/gl51/projet/java/StudentController.java
@@ -2,7 +2,7 @@ package gl51.projet.java;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.HttpStatus;
+
 
 @Controller("/student")
 public class StudentController {
